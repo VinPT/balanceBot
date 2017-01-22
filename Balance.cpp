901fc996d3ff767e348b30a571c8fb074baa9d55
William Wodrich <wodrichw@oregonstate.edu>
2017-01-21 17:11:50 -0800

    fixed integral calculation

--- Balance.cpp
+++ Balance.cpp
@@ -8,6 +8,7 @@ Balance::Balance(){
 	proportion = 0;
 	integral = 0;
 	spd = 0;
+	calc_spd_count = 0;
 
 	/*
 	 *below coefficients are adjusted to control the pi controller
@@ -24,8 +25,9 @@ void Balance::calc_spd(){
 	//angle_acc = get_angle_acc(); !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	//elapsed_time = get_elapsed_time(); !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	//desired_angle = get_desired_angel(); !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	calc_spd_count++;
 	proportion = calc_angle() - desired_angle; //could need to be calc_angle() + desired_angle;
-	integral = elapsed_time*proportion;
+	integral = elapsed_time*proportion/calc_spd_count;
 	spd = p_coeff*proportion + i_coeff*integral;
 }
 

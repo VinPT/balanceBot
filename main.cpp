57a79880b0e992d2b4d5d744277c961977a016fb
William Wodrich <wodrichw@oregonstate.edu>
2017-01-22 02:15:21 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 
 #include "main.h"
+#include "Balance.h"
+#include "Controls.h"
 
 
 
@@ -7,5 +9,15 @@ int main()
 {
 	noecho();//ncurses
 	initscr();//ncurses
-		
+	timeout(10);//ncurses -- waits 10 ms for input
+	//int mili_count = get_clock() + 100;
+	char wasd;
+	Balance B;
+	while(1){
+		wasd = getch();
+		if(get_clock() = mili_count){
+			B.move(wasd);
+		}
+	}
+	return 0;
 }

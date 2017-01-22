2ab79aa45f47519ee0b6eb854ad656d391be4328
William Wodrich <wodrichw@oregonstate.edu>
2017-01-21 16:25:03 -0800

    Update Balance.h

--- Balance.h
+++ Balance.h
@@ -1,1 +1,23 @@
+#ifndef BALANCE_H
+#define BALANCE_H
 
+class Balance{
+private:
+	float angle_init;
+	float angle_acc;
+	float elapsed_time;
+	float proportion;
+	float intagral;
+	float p_coeff;
+	float i_coeff;
+	float spd;
+
+
+	float calc_spd();
+	float calc_angle();
+public:
+	Balance();
+	~Balance();
+	float get_spd();
+};
+#endif

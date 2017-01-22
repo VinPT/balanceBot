5f7595e98af76c0a172b5ba0f537ce2f53637395
William Wodrich <wodrichw@oregonstate.edu>
2017-01-22 11:08:58 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-
+#include <wiringpi.h>
 #include "main.h"
 #include "Balance.h"
 #include "Controls.h"
@@ -10,13 +10,13 @@ int main()
 	noecho();//ncurses
 	initscr();//ncurses
 	timeout(10);//ncurses -- waits 10 ms for input
-	//int mili_count = get_clock() + 100;
+	int mili_count = millis() + 100;
 	char wasd;
 	Balance B;
 	Stepper step;
 	while(1){
 		wasd = getch();
-		if(get_clock() = mili_count){
+		if(millis() = mili_count){
 			mili_count += 100;
 			B.move(wasd);
 			step.set_steer(B.get_steer());			
